254a5494ce2595f4c07b126b51fb166c3c0046ca
Satish-Thorati <satishthorati09@gmail.com>
2026-02-23 09:05:56 +0530

    Polymorphism

---
+++ Practice_Programs/OOps/26.Polymorphism.py
@@ -0,0 +1,20 @@
+#26. Login Systems
+
+class LoginSystem:
+    def login(self):
+       pass
+
+class GoogleLogin(LoginSystem):
+    def login(self):
+        print("Logged in using Google")
+
+class FacebookLogin(LoginSystem):
+    def login(self):
+        print("Logged in using Facebook")
+
+class FacebookLogin(LoginSystem):
+    def login(self):
+        print("Logged in using Facebook")
+
+GoogleLogin().login()
+FacebookLogin().login()
